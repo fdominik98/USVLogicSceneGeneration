7e6c3e94a665b2d9559a42cefce110f04c5de673
Dominik Frey <domfr93@liu.se>
2024-12-10 15:27:41 +0100

    neighborhood

--- src/functional_level/metamodels/functional_scenario.py
+++ src/functional_level/metamodels/functional_scenario.py
@@ -1,14 +1,14 @@
 from dataclasses import dataclass, field
 from itertools import combinations
-from typing import List, Union
+from typing import Any, Dict, List, Set, Tuple, Union
 from functional_level.metamodels.relation_class import RelationClass, RelationClassClause
 from functional_level.metamodels.vessel_class import FuncObject, VesselClass
 from logical_level.models.relation_types import OutVisOrNoCollide
 import copy
 from functional_level.metamodels.interpretation import (
-    HeadOnInterpretation, OvertakingInterpretation, CrossingFromPortInterpretation, OSInterpretation, TSInterpretation)
+    HeadOnInterpretation, Interpretation, OvertakingInterpretation, CrossingFromPortInterpretation, OSInterpretation, TSInterpretation)
 
-class FunctionalScenario():
+class FunctionalScenario2():
     def __init__(self, group : str, id, func_objects : List[VesselClass], relation_desc_clauses : Union[List[RelationClassClause], List[RelationClass]]) -> None:
         self.group = group
         self.id = id
@@ -35,7 +35,7 @@ class FunctionalScenario():
         return len(self.func_objects)
     
 @dataclass(frozen=True)
-class FunctionalScenario2():
+class FunctionalScenario():
     group : str
     id : str
     os_interpretation : OSInterpretation = OSInterpretation()
@@ -46,11 +46,101 @@ class FunctionalScenario2():
     
     func_objects : List[FuncObject] = field(init=False)
     
-    self.vessel_objects = sorted(vessel_objects, key=lambda x: x.id)
+    def __post_init__(self):
+        object_set : Set[FuncObject] = set()
+        for interpretations in [self.os_interpretation, self.ts_interpretation, self.head_on_interpretations, self.overtaking_interpretations, self.crossing_interpretations]:
+            for interpretation in interpretations:
+                for func_object in interpretation:
+                    object_set.add(func_object)
+        object.__setattr__(self, 'func_objects', sorted(list(object_set), key=lambda x: x.id))
+        head_on_interpretation_temp = copy(self.head_on_interpretations)
+        for tup in head_on_interpretation_temp:
+            self.head_on_interpretations.add(tup)
+        
     
     @property
     def name(self):
-        return f'{str(len(self.func_objects))}vessel_{self.group}_{id}'
+        return f'{str(len(self.func_objects))}vessel_{self.group}_{self.id}'
+    
+    def in_colreg_rel(self, o1 : FuncObject, o2 : FuncObject) -> bool:
+        return self.colreg_rel(o1, o2) or self.colreg_rel(o2, o1)
+    
+    def in_overtaking(self, o1 : FuncObject, o2 : FuncObject) -> bool:
+        return self.overtaking(o1, o2) or self.overtaking(o2, o1)
+    
+    def in_crossing(self, o1 : FuncObject, o2 : FuncObject) -> bool:
+        return self.crossing(o1, o2) or self.crossing(o2, o1)
+    
+    def colreg_rel(self, o1 : FuncObject, o2 : FuncObject) -> bool:
+        return self.head_on(o1, o2) or self.overtaking(o1, o2) or self.crossing(o1, o2)
+    
+    def head_on(self, o1 : FuncObject, o2 : FuncObject) -> bool:
+        return self.head_on_interpretations.contains((o1, o2))
+    
+    def overtaking(self, o1 : FuncObject, o2 : FuncObject) -> bool:
+        return self.overtaking_interpretations.contains((o1, o2))
+    
+    def crossing(self, o1 : FuncObject, o2 : FuncObject) -> bool:
+        return self.crossing_interpretations.contains((o1, o2))
+    
+    @property
+    def all_object_pairs(self) -> Set[Tuple[FuncObject, FuncObject]]:
+        return {(oi, oj) for oi, oj in combinations(self.func_objects, 2)}
+    
+    @property
+    def not_in_colreg_pairs(self):
+        not_in_colreg_pairs : Set[Tuple[FuncObject, FuncObject]] = set()
+        for o1, o2 in self.not_in_colreg_pairs:
+            if not self.in_colreg_rel(o1, o2) and (o1, o2) not in not_in_colreg_pairs and (o2, o1) not in not_in_colreg_pairs:
+                not_in_colreg_pairs.add(o1, o2)
+        return not_in_colreg_pairs
+    
+    def shape_hash(self, hops: int = 1):
+        """
+        Compute the hash of the node attributes and their neighborhoods up to the given number of hops.
+        
+        :param hops: Number of hops to consider in the neighborhood.
+        :return: Hash of the neighborhood information.
+        """
+        # Initialize a dictionary to store neighborhoods for all hop levels
+        neighborhoods: Dict[int, Dict[FuncObject, Tuple]] = {0: {}}
+        
+        # Initialize base (0-hop) attributes
+        for node in self.func_objects:
+            attributes: Set[tuple] = set()
+            if self.os_interpretation.contains(node):
+                attributes.add((OSInterpretation.name, 1))
+            if self.ts_interpretation.contains(node):
+                attributes.add((TSInterpretation.name, 1))
+            relation_descs = self.overtaking_interpretations.get_relation_descs(node)
+            attributes.update((rd[0], rd[1], None) for rd in relation_descs)
+            relation_descs = self.head_on_interpretations.get_relation_descs(node)
+            attributes.update((rd[0], rd[1], None) for rd in relation_descs)
+            relation_descs = self.crossing_interpretations.get_relation_descs(node)
+            attributes.update((rd[0], rd[1], None) for rd in relation_descs)
+            neighborhoods[0][node] = (None, frozenset(attributes))
+
+        # Build neighborhoods iteratively for each hop
+        for hop in range(1, hops + 1):
+            neighborhoods[hop] = {}
+            for node in self.func_objects:
+                attributes: Set[tuple] = set()
+                if self.os_interpretation.contains(node):
+                    attributes.add((OSInterpretation.name, 1))
+                if self.ts_interpretation.contains(node):
+                    attributes.add((TSInterpretation.name, 1))
+                relation_descs = self.overtaking_interpretations.get_relation_descs(node)
+                attributes.update((rd[0], rd[1], neighborhoods[hop - 1][rd[2]]) for rd in relation_descs)
+                relation_descs = self.head_on_interpretations.get_relation_descs(node)
+                attributes.update((rd[0], rd[1], neighborhoods[hop - 1][rd[2]]) for rd in relation_descs)
+                relation_descs = self.crossing_interpretations.get_relation_descs(node)
+                attributes.update((rd[0], rd[1], neighborhoods[hop - 1][rd[2]]) for rd in relation_descs)
+                neighborhoods[hop][node] = (neighborhoods[hop - 1][node], frozenset(attributes))
+
+        # Return hash of the last neighborhood
+        return hash(frozenset(neighborhoods[hops].values()))
+            
+    
                     
 class MSREnvironmentDesc(FunctionalScenario):
     group = 'MSR'
--- src/functional_level/metamodels/interpretation.py
+++ src/functional_level/metamodels/interpretation.py
@@ -1,34 +1,67 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import List, Tuple
+from typing import Optional, Set, Tuple
 
 from src.functional_level.metamodels.vessel_class import FuncObject
 
 @dataclass(frozen=True)
 class Interpretation(ABC):
-    @abstractmethod
-    def add(self, object):
-        pass
+    name : str = ''
+    _data : Set[tuple]
+    
+    def __len__(self):
+        return len(self._data)
+
+    def __iter__(self):
+        return iter(self._data)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self._data})"
     
     @abstractmethod
-    def contains(self, object) -> bool:
+    def contains(self, objects) -> bool:
         pass
 
-
+    @abstractmethod
+    def add(self, objects):
+        pass
+        
 @dataclass()
 class UnaryInterpretation(Interpretation, ABC):
-    _data : List[Tuple[FuncObject]]
+    _data : Set[Tuple[FuncObject]]
     
     def contains(self, object : FuncObject) -> bool:
         return (object,) in self._data
     
+    def add(self, object : FuncObject):
+        self._data.add((object,))       
+   
+    
 @dataclass()
 class BinaryInterpretation(Interpretation, ABC):
-    _data : List[Tuple[FuncObject, FuncObject]]
+    _data : Set[Tuple[FuncObject, FuncObject]]
     
     def contains(self, objects : Tuple[FuncObject, FuncObject]) -> bool:
         return objects in self._data
     
+    def add(self, objects : Tuple[FuncObject, FuncObject]):
+        self._data.add(objects)
+        
+    def get_tuples(self, o1 : Optional[FuncObject] = None, o2 : Optional[FuncObject] = None):
+        tuples : Set[Tuple[FuncObject, FuncObject]] = set()
+        for _o1, _o2 in self._data:
+            if (o1 == _o1 or None) and (o2 == _o2 or None):
+                tuples.add((_o1, _o2))
+        return tuples
+    
+    def get_relation_descs(self, o : FuncObject) -> Set[Tuple[str, int, FuncObject]]:
+        descs : Set[Tuple[str, int, FuncObject]] = set()
+        for _o1, _o2 in self._data:
+            if _o1 == o:
+                descs.add((self.name, 1, _o2))
+            elif _o2 == o:
+                descs.add((self.name, 2, _o1))
+        return descs
     
 @dataclass()
 class VesselInterpretation(UnaryInterpretation, ABC):
@@ -36,22 +69,22 @@ class VesselInterpretation(UnaryInterpretation, ABC):
 
 @dataclass()
 class OSInterpretation(VesselInterpretation):
-    pass
+    name : str = 'OS'
 
 @dataclass()
 class TSInterpretation(VesselInterpretation):
-    pass
+    name : str = 'TS'
 
 @dataclass()
 class HeadOnInterpretation(BinaryInterpretation):
-    pass
+    name : str = 'HeadOn'
 
 @dataclass()
 class CrossingFromPortInterpretation(BinaryInterpretation):
-    pass
+    name : str = 'Crossing'
 
 @dataclass()
 class OvertakingInterpretation(BinaryInterpretation):
-    pass
+    name : str = 'Overtaking'
 
 
