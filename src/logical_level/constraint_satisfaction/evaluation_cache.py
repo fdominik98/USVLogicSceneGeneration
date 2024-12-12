cb0086b72a28847b8d3c3958c24f358bba4f17b7
Frey Dominik <frey.dominik@hotmail.hu>
2024-12-12 11:42:43 +0100

    refactor

---
+++ src/logical_level/constraint_satisfaction/evaluation_cache.py
@@ -0,0 +1,47 @@
+from typing import Dict, Tuple
+
+import numpy as np
+from asv_utils import EPSILON, N_MILE_TO_M_CONVERSION, o2VisibilityByo1
+from logical_level.constraint_satisfaction.assignments import Assignments
+from logical_level.models.values import Values
+from logical_level.models.vessel_variable import VesselVariable
+
+
+class GeometricProperties():
+    def __init__(self, var1 : VesselVariable, var2 : VesselVariable, assignments : Assignments):
+        self.val1 : Values = assignments(var1)
+        self.val2 : Values = assignments(var2)
+        self.safety_dist = max(self.val1.r, self.val2.r)
+        self.p12 = self.val2.p - self.val1.p
+        self.p21 = self.val1.p - self.val2.p
+        self.v12 = self.val1.v - self.val2.v
+        
+        # Define the norm of the relative position (distance(p1 p2))
+        self.o_distance = max(np.linalg.norm(self.p12), EPSILON)   
+        
+        self.cos_p21_v2_theta = np.clip(np.dot(self.p21, self.val2.v) / self.o_distance / self.val2.sp, -1, 1)
+        self.angle_p21_v2 = np.arccos(self.cos_p21_v2_theta)        
+        self.cos_p12_v1_theta = np.clip(np.dot(self.p12, self.val1.v) / self.o_distance / self.val1.sp, -1, 1)
+        self.angle_p12_v1 = np.arccos(self.cos_p12_v1_theta)
+        
+        self.vis_distance = min(o2VisibilityByo1(self.angle_p12_v1, self.val1.l),
+                           o2VisibilityByo1(self.angle_p21_v2, self.val2.l)) *  N_MILE_TO_M_CONVERSION
+        # angle between the relative velocity and the relative position vector
+        
+        self.v12_norm_stable = max(np.linalg.norm(self.v12), EPSILON)
+        self.dot_p12_v12 = np.dot(self.p12, self.v12)
+        #self.cos_p12_v12_theta = np.clip(self.dot_p12_v12 / self.o_distance / self.v12_norm_stable, -1, 1)
+        #self.angle_v12_p12 = np.arccos(self.cos_p12_v12_theta)
+        
+        self.tcpa = self.dot_p12_v12 / self.v12_norm_stable**2
+        self.dcpa = np.linalg.norm(self.p21 + self.v12 * max(0, self.tcpa)) 
+
+class EvaluationCache(Dict[Tuple[VesselVariable, VesselVariable], GeometricProperties]):
+    def __init__(self, assignments : Assignments, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.assignments = assignments    
+    
+    def get_props(self, var1 : VesselVariable, var2 : VesselVariable):
+        if (var1, var2) not in self:
+            self[(var1, var2)] = GeometricProperties(var1, var2, self.assignments)
+        return self.get((var1, var2))
--- src/logical_level/models/literal.py
+++ src/logical_level/models/literal.py
@@ -1,8 +1,128 @@
 
-from dataclasses import dataclass
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+
+import numpy as np
+
+from asv_utils import BEAM_ANGLE, BOW_ANGLE, DIST_DRIFT, MASTHEAD_LIGHT_ANGLE, MAX_DISTANCE
+from logical_level.constraint_satisfaction.assignments import Assignments
+from logical_level.constraint_satisfaction.evaluation_cache import EvaluationCache, GeometricProperties
+from logical_level.models.penalty import Penalty
+from logical_level.models.vessel_variable import VesselVariable
 
 
 dataclass(frozen=True)
-class Literal():
-    negated : bool
+class Literal(ABC):
+    var1 : VesselVariable
+    var2 : VesselVariable
+    literal_type : str
+    max_value : float
+    negated : bool = False
+    
+    @property
+    def name(self):
+        prefix = "!" if self.negated else ""
+        return prefix + self.literal_type
+    
+    def evaluate_penalty(self, eval_cache : EvaluationCache) -> Penalty:
+        return self._do_evaluate_penalty(eval_cache.get_props(self.var1, self.var2))
+        
+    @abstractmethod
+    def _do_evaluate_penalty(self, geo_props : GeometricProperties) -> Penalty:
+        pass
+    
+    def penalty(self, val, lb, ub) -> float:
+        if not self.negated:
+            dist = self._penalty(val, lb, ub)
+            return self._normalize(dist, lb, ub)
+        else:
+            dist1 = self._penalty(val, 0, lb)
+            dist2 = self._penalty(val, ub, self.max_value)
+            return min(self._normalize(dist1, 0, lb), self._normalize(dist2, ub, self.max_value))
+    
+    def _penalty(self, val, lb, ub):
+        if val < lb:
+            distance =  lb - val
+        elif val > ub:
+            distance = val - ub
+        else:
+            return 0.0
+        return distance
+    
+    def _normalize(self, dist, lb, ub) -> float:
+        return dist / (max(lb, self.max_value - ub))
+    
+    
+############ VISIBILITY DISTANCE ##################
+@dataclass(frozen=True)
+class AtVis(Literal):
+    literal_type : str = field(default='AtVis', init=False)
+    max_value : float = field(default=MAX_DISTANCE, init=False)
+    
+    def _do_evaluate_penalty(self, geo_props : GeometricProperties) -> Penalty:
+        return Penalty([self.penalty(geo_props.o_distance,
+                                     geo_props.vis_distance - DIST_DRIFT,
+                                     geo_props.vis_distance + DIST_DRIFT)], [], [])
+        
+@dataclass(frozen=True)
+class InVis(Literal):
+    literal_type : str = field(default='InVis', init=False)
+    max_value : float = field(default=MAX_DISTANCE, init=False)
+    
+    def _do_evaluate_penalty(self, geo_props : GeometricProperties) -> Penalty:
+        return Penalty([self.penalty(geo_props.o_distance,
+                                     geo_props.safety_dist,
+                                     geo_props.vis_distance)], [], [])
+        
+@dataclass(frozen=True)
+class OutVis(Literal):
+    literal_type : str = field(default='OutVis', init=False)
+    max_value : float = field(default=MAX_DISTANCE, init=False)
+    
+    def _do_evaluate_penalty(self, geo_props : GeometricProperties) -> Penalty:
+        return Penalty([self.penalty(geo_props.o_distance, 
+                                     max(geo_props.vis_distance, geo_props.safety_dist),
+                                     MAX_DISTANCE)], [], [])
+        
+        
+############ COLREG RELATIVE BEARING ##################       
+class CrossingBear(Literal):
+    literal_type : str = field(default='CrossingBear', init=False)
+    max_value : float = field(default=np.pi, init=False)
+    rotation_angle : float = field(default=(BOW_ANGLE + BEAM_ANGLE) / 2, init=False)
+        
+    def _do_evaluate_penalty(self, geo_props : GeometricProperties) -> Penalty:
+        angle_p21_v2_rot = np.arccos(np.dot(geo_props.p21, self.__rotated_v2(geo_props)) /
+                                     geo_props.o_distance / geo_props.val2.sp)
+        return Penalty([], [(self.penalty(angle_p21_v2_rot, 0.0, BEAM_ANGLE / 2.0)
+                            + self.penalty(geo_props.angle_p12_v1, 0.0, MASTHEAD_LIGHT_ANGLE / 2.0))], [])
+        
+    def __rotated_v2(self, geo_props : GeometricProperties):
+        rotation_matrix = np.array([
+            [np.cos(self.rotation_angle), -np.sin(self.rotation_angle)],
+            [np.sin(self.rotation_angle), np.cos(self.rotation_angle)]
+        ])
+        # Rotate vector
+        return np.dot(rotation_matrix, geo_props.val2.v)
+    
+    
+
+class HeadOnBear(Literal):
+    literal_type : str = field(default='HeadOnBear', init=False)
+    max_value : float = field(default=np.pi, init=False)
+        
+    def _do_evaluate_penalty(self, geo_props : GeometricProperties) -> Penalty:
+        return Penalty([], [self.penalty(geo_props.angle_p21_v2, 0.0, BOW_ANGLE / 2.0)
+                            + self.penalty(geo_props.angle_p12_v1, 0.0, BOW_ANGLE / 2.0)], [])
+        
+
+class OvertakingBear(Literal):
+    literal_type : str = field(default='OvertakingBear', init=False)
+    max_value : float = field(default=np.pi, init=False)
+        
+    def _do_evaluate_penalty(self, geo_props : GeometricProperties) -> Penalty:
+        return (self.penalty(geo_props.angle_p21_v2, MASTHEAD_LIGHT_ANGLE / 2.0, np.pi)
+                + self.penalty(geo_props.angle_p12_v1, 0.0, MASTHEAD_LIGHT_ANGLE / 2.0))
+    
+    
     
